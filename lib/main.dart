a246f2b01b58f9aee7a81ca4d2af5e61e5941f43
cvc953 <cvcorporation05@gmail.com>
2025-11-29 01:04:29 -0500

    cambios en el main para inicializar metadata_god

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:lyrio/screens/home_screen.dart';
+import 'package:metadata_god/metadata_god.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await MetadataGod.initialize(); // 🔥 obligatorio
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
-    );
-  }
+  runApp(const HomeScreen());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lyrio/main.dart';
+import 'package:lyrio/main.dart2';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
