ea8b1c0710c44366b21ae6df001cedd5875efba8
cvc953 <cvcorporation05@gmail.com>
2025-11-30 15:34:01 -0500

    workfow terminado 2

--- lib/utils/permissions.dart
+++ lib/utils/permissions.dart
@@ -3,14 +3,16 @@ import 'package:permission_handler/permission_handler.dart';
 class AppPermissions {
   static Future<bool> requestStorage() async {
     // Android 13+
-    final audio = await Permission.audio.request();
+    final audio = await Permission.mediaLibrary.request();
     if (audio.isGranted) return true;
 
-    // Android 11+: MANAGE_EXTERNAL_STORAGE
-    final manage = await Permission.manageExternalStorage.request();
-    if (manage.isGranted) return true;
+    final photos = await Permission.photos.request();
+    if (photos.isGranted) return true;
 
-    // Android < 10
+    final videos = await Permission.videos.request();
+    if (videos.isGranted) return true;
+
+    // Android 12 o menos
     final storage = await Permission.storage.request();
     return storage.isGranted;
   }
