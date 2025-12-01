aa7d00167090137ec9db82075f07c3054763dc20
cvc953 <cvcorporation05@gmail.com>
2025-12-01 07:02:59 -0500

    workfow 8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.cvc953.lyrio"
+    namespace = "com.example.lyrio"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.cvc953.lyrio"
+        applicationId = "com.example.lyrio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         //minSdk = flutter.minSdkVersion
